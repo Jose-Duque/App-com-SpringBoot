b839b1cbe2567df0709ec9c73e3a2e0874c4e88c
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-11-22 19:02:40 -0300

    Add CategoryDTO

--- backend/src/main/java/com/jcduque/resources/CategoryResource.java
+++ backend/src/main/java/com/jcduque/resources/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.jcduque.resources;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jcduque.entities.Category;
+import com.jcduque.dto.CategoryDTO;
 import com.jcduque.services.CategoryService;
 
 @RestController
@@ -20,9 +20,9 @@ public class CategoryResource {
 	CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		
-		List<Category> list = service.finsAll();
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/jcduque/services/CategoryService.java
+++ backend/src/main/java/com/jcduque/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.jcduque.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jcduque.dto.CategoryDTO;
 import com.jcduque.entities.Category;
 import com.jcduque.repositories.CategoryRepository;
 
@@ -15,10 +17,10 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository repository;
 
-	
-	public List<Category> finsAll() {
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
 		
 		List<Category> list = repository.findAll();
-		return list;
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
