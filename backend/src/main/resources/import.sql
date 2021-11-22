53ac45c2356b1ede22ef2e60ee28184a5a980e40
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-11-22 18:17:29 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_category (name) VALUES ('LIVOS');
+INSERT INTO tb_category (name) VALUES ('Pizza Bacon');
+INSERT INTO tb_category (name) VALUES ('Pizza Moda da Casa');
+INSERT INTO tb_category (name) VALUES ('Pizza Portuguesa');
+INSERT INTO tb_category (name) VALUES ('Risoto de Carne');
+INSERT INTO tb_category (name) VALUES ('Risoto Funghi');
+INSERT INTO tb_category (name) VALUES ('Macarrão Espaguete');
+INSERT INTO tb_category (name) VALUES ('Macarrão Fusili');
+INSERT INTO tb_category (name) VALUES ('Macarrão Penne');
