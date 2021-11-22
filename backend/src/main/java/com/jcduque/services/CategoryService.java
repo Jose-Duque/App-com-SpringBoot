b86ebaa2c1a9f1180026c1b999115c01d9483171
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-11-21 21:35:52 -0300

    Add H2 Database

---
+++ backend/src/main/java/com/jcduque/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.jcduque.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jcduque.entities.Category;
+import com.jcduque.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	CategoryRepository repository;
+
+	public List<Category> finsAll() {
+		
+		List<Category> list = repository.findAll();
+		return list;
+	}
+}
